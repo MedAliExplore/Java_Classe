678c2f58db4a2b8b5839d38eab2371a66803c61b
maboudlal <boudlalmohamedali@gmail.com>
2025-09-22 10:50:52 +0200

    factorisation des methodes

--- src/GestionCentreFormation/Exec.java
+++ src/GestionCentreFormation/Exec.java
@@ -11,34 +11,22 @@ public class Exec {
         Double moyenneEtudiant = 0.0;
         Map<String,ArrayList<Double>> resultats = new HashMap<>();
 
-        //Création d'une liste de modules avec leurs coefficient
-        Map<String,Integer> listeModulesCoef = new HashMap<>();
-        listeModulesCoef.put("PHYSIQUE",2);
-        listeModulesCoef.put("MATH",5);
-        listeModulesCoef.put("ANGLAIS",1);
-
-        
         //Création de la formation avec ses modules
-        Curruculum formation1 = new Curruculum("ETUDIANT001",listeModulesCoef);
-
-        //ajout notes
-        ArrayList<Double> listeNotes = new ArrayList<>();
-        listeNotes.add(0,12.0);
-        listeNotes.add(1,13.0);
-        listeNotes.add(2,14.0);
-
-        resultats.put("PHYSIQUE",listeNotes);
-        resultats.put("MATH",listeNotes);
-        resultats.put("ANGLAIS",listeNotes);
+        Curruculum formation1 = new Curruculum();
+        formation1.addModCoef("PHYSIQUE", 2);
+        formation1.addModCoef("MATH", 5);
+        formation1.addModCoef("ANGLAIS", 2);
 
         //Création d'un étudiant et assignation vers une formation
         Student etudiant1 = new Student("IDETUDIANT001",formation1,resultats);
-
-
-        Boolean res =  etudiant1.setNoteMod("PHYSIQUE", 10.0);
-        etudiant1.setNoteMod("MATH", 19.0);
-        etudiant1.setNoteMod("ANGLAIS", 15.0);
-        System.out.println("le module est ajouté"+res);
+        Boolean res =  false;
+        res = etudiant1.setNoteMod("PHYSIQUE",15.0);
+        System.out.println("le module est-il été ajouté = "+res);
+        res = etudiant1.setNoteMod("MATH", 19.0);
+        System.out.println("le module est-il été ajouté = "+res);
+        res = etudiant1.setNoteMod("ANGLAIS", 18.0);
+        System.out.println("le module est-il été ajouté = "+res);
+        
         moyenneEtudiant = etudiant1.moyenneGenerale(etudiant1,formation1 );
 
         System.out.println("La moyenne generale de l'étudiant est : "+moyenneEtudiant);
